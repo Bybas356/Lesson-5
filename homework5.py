47ec688ae28031facdd9f5386cc5ef6544944cd6
Maxim <boss.mirkovskiy@gmail.com>
2024-07-03 16:15:19 +0700

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,15 @@
+immutable_var = (0, 2, 'Бубас', False)
+print('Immutable tuple:', immutable_var)
+#immutable_var[1] = 'Кукусик'
+#print(immutable_var) - выдаёт ошибку, так как переменной immutable_var присвоен кортеж - неизменяемый объект, то изменить его нельзя
+#print(type(immutable_var)) <class 'tuple'>
+mutable_list = [0, 2, 'Бубас', True]
+print('Mutable list:', mutable_list)
+mutable_list[0] = 'Пупырка'
+print('Mutable list modified:', mutable_list)
+mutable_list.append(False)
+print('Mutable list modified:', mutable_list)
+mutable_list.extend('apple')
+print('Mutable list modified:', mutable_list)
+mutable_list.remove('a')
+print('Mutable list modified:', mutable_list)
